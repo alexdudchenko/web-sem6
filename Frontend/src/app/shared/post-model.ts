51b192cb024be878407e53be8bad8e5c558c9634
alexproudmur <alexproudmur@gmail.com>
2023-06-09 10:27:30 +0300

    Frontend

---
+++ Frontend/src/app/shared/post-model.ts
@@ -0,0 +1,9 @@
+export class PostModel {
+    id: number;
+    postName: string;
+    description: string;
+    userName: string;
+    subredditName: string;
+    commentCount: number;
+    duration: string;
+}
